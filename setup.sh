88e1c54597764f19d7bd6410ee2cd0ebf417aef3
flyw <690232+flyw@users.noreply.github.com>
2020-03-18 15:17:53 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+opkg update
+opkg install openssh-server openssh-client openssh-keygen autossh socat redsocks ipset
+
+# Install libustream tls
+# opkg list | grep libustream | grep tls
+
+tls_line=$(opkg list | grep libustream | grep tls)
+tls_package=$(echo "$tls_line"  | head -n1 | cut -d " " -f1)
+opkg install ${tls_package}
+
+ssh-keygen
