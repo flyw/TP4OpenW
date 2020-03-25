18818dc199b5d732108734600cdbc01d9027cbff
wang.yuan <wang.yuan@joydata.com>
2020-03-25 13:56:32 +0800

    update

--- installation.sh
+++ installation.sh
@@ -3,14 +3,14 @@
 opkg remove dnsmasq
 opkg install dnsmasq-full
 opkg install openssh-client openssh-keygen autossh redsocks ipset iptables-mod-nat-extra ttyd luci-app-ttyd
-#opkg install socat
+opkg install socat
 
 # Install libustream tls
 # opkg list | grep libustream | grep tls
 
-tls_line=$(opkg list | grep libustream | grep tls)
-tls_package=$(echo "$tls_line"  | head -n1 | cut -d " " -f1)
-opkg install ${tls_package}
+#tls_line=$(opkg list | grep libustream | grep tls)
+#tls_package=$(echo "$tls_line"  | head -n1 | cut -d " " -f1)
+#opkg install ${tls_package}
 
 cp etc/rc.local /etc/rc.local
 cp etc/redsocks.conf /etc/redsocks.conf
--- update_conf.sh
+++ update_conf.sh
@@ -12,7 +12,7 @@ fi
 
 echo 'Generate /tmp/dnsmasq.d/gfwlist.conf...'
 echo '' > /tmp/dnsmasq.d/gfwlist.conf
-sed "s/.*/server=\/&\/[UPSTREAM_IP]#[UPSTREAM_PORT]/" $file >> /tmp/dnsmasq.d/gfwlist.conf
+sed "s/.*/server=\/&\/127.0.0.1#55/" $file >> /tmp/dnsmasq.d/gfwlist.conf
 sed "s/.*/ipset=\/&\/gfwlist/" $file >> /tmp/dnsmasq.d/gfwlist.conf
 echo "Success."
 
