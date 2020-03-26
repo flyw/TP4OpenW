a75dc4f1a490b8feb6940dcd7974e0724e059990
wang.yuan <wang.yuan@joydata.com>
2020-03-26 17:28:49 +0800

    update

--- installation.sh
+++ installation.sh
@@ -8,9 +8,9 @@ opkg install socat
 # Install libustream tls
 # opkg list | grep libustream | grep tls
 
-#tls_line=$(opkg list | grep libustream | grep tls)
-#tls_package=$(echo "$tls_line"  | head -n1 | cut -d " " -f1)
-#opkg install ${tls_package}
+tls_line=$(opkg list | grep libustream | grep tls)
+tls_package=$(echo "$tls_line"  | head -n1 | cut -d " " -f1)
+opkg install ${tls_package}
 
 cp etc/rc.local /etc/rc.local
 cp etc/redsocks.conf /etc/redsocks.conf
