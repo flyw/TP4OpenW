821ab2774da386748408116a3229ef9179eddc8f
wang.yuan <wang.yuan@joydata.com>
2020-03-30 13:20:05 +0800

    update

--- server/installation.sh
+++ server/installation.sh
@@ -3,5 +3,15 @@ apt update
 apt install autossh dante-server socat -y
 systemctl enable danted
 
+cp danted.conf /etc/danted.conf
+cp startService.sh /etc/startService.sh
+chmod a+x /root/startService.sh
+systemctl restart danted
+
+line="0 * * * * killall -9 socat; socat tcp4-listen:5353,bind=127.0.0.1,reuseaddr,fork UDP:127.0.0.53:53 &"
+(crontab -u root -l; echo "$line" ) | crontab -u root -
+line="@reboot /root/startService.sh"
+(crontab -u root -l; echo "$line" ) | crontab -u root -
+
 ssh-keygen
 ssh-copy-id [%]
---
+++ server/startService.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+touch /tmp/system-startup
+
+socat tcp4-listen:5353,bind=127.0.0.1,reuseaddr,fork UDP:127.0.0.53:53 &
+autossh -M 60102 -f -C -R 49080:localhost:1080 -o ServerAliveInterval=30 -o ServerAliveCountMax=2 -N root@[DOWNSTREAM_IP]
+autossh -M 60103 -f -C -R 5353:localhost:5353 -o ServerAliveInterval=30 -o ServerAliveCountMax=2 -N root@[DOWNSTREAM_IP]
+
+echo "reboot" | at 5:00
