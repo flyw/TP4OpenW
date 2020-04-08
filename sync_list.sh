645aa793afd3dc9aecbc932e3210298b8d6467e8
wang.yuan <wang.yuan@joydata.com>
2020-04-08 11:38:35 +0800

    update

---
+++ sync_list.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/flyw/TP4OpenW/master/lists/list  -O /tmp/tmp/list -q
+
+NEWFILE=/tmp/tmp/list
+OLDFILE=/root/TP4OpenW/lists/list
+if test -f "$NEWFILE"; then
+    if cmp -s "$OLDFILE" "$NEWFILE" ; then
+        echo "Nothing changed."
+    else
+        mv "$NEWFILE" "$OLDFILE"
+        echo "List synced."
+    fi
+fi
