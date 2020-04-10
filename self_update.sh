9587a35f03d834967bdce3f8476cd22002aa831a
wang.yuan <wang.yuan@joydata.com>
2020-04-10 22:00:08 +0800

    update

--- self_update.sh
+++ self_update.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 wget https://github.com/flyw/TP4OpenW/archive/master.zip -O /root/master.zip
-cd /root
-unzip master.zip
-mv TP4OpenW-master TP4OpenW
+unzip /root/master.zip -d /root
+mv /root/TP4OpenW-master /root/TP4OpenW
 cp /root/TP4OpenW/etc/crontabs/root /etc/crontabs/root
