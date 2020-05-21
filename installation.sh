550d59e21406f01972f87560b5ac6f4b97072bc1
wang.yuan <wang.yuan@joydata.com>
2020-05-21 15:56:20 +0800

    update

--- installation.sh
+++ installation.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 opkg install openssh-client openssh-keygen autossh redsocks ipset iptables-mod-nat-extra ttyd luci-app-ttyd isc-dhcp-server-ipv4 luci-app-unbound
-opkg remove dnsmasq
+sed -i 's/=53/=45355/g' /usr/lib/unbound/unbound.sh
+sed -i 's/ 53/ 45355/g' /usr/lib/unbound/unbound.sh
+sed -i 's/"53"/"45355"/g' /usr/lib/unbound/unbound.sh
+sed -i 's/echo "server:"/echo "server:"\n    echo "  tcp-upstream: yes"/g' /usr/lib/unbound/unbound.sh
+sed -i 's/echo "server:"/echo "server:"\n    echo "  do-not-query-localhost: no"/g' /usr/lib/unbound/unbound.sh
+sed -i 's/cp -p \/etc\/unbound\/\* $UB_VARDIR\//cp -p \/etc\/unbound\/* $UB_VARDIR\/\n  \/root\/TP4OpenW\/update_conf.sh/g' /usr/lib/unbound/unbound.sh
 /etc/init.d/unbound restart
 
 cp /root/TP4OpenW/etc/rc.local /etc/rc.local
--- update_conf.sh
+++ update_conf.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 
 #forward-zone:
-#        name: "baidu.com"
-#        forward-addr: 192.168.99.1@45353
+#        name: "google.com"
+#        forward-addr: 127.0.0.1@45353
 #forward-zone:
 #        name: "sina.com.cn"
 #        forward-addr: 192.168.99.1@45353
 
-echo 'Generate /var/lib/unbound/unbound_ext.conf...'
-cp /etc/unbound/unbound_ext.conf /var/lib/unbound/unbound_ext.conf
-chown unbound:unbound /var/lib/unbound/unbound_ext.conf
+#do-not-query-localhost: no
+
+# echo 'Generate /var/lib/unbound/unbound_ext.conf...'
+# cp /etc/unbound/unbound_ext.conf /var/lib/unbound/unbound_ext.conf
+# chown unbound:unbound /var/lib/unbound/unbound_ext.conf
 
 sed "s/.*/forward-zone:\n        name: \"&\"\n        forward-addr: 127.0.0.1@45353/" /root/TP4OpenW/lists/list >> /var/lib/unbound/unbound_ext.conf
 sed "s/.*/forward-zone:\n        name: \"&\"\n        forward-addr: 127.0.0.1@45353/" /root/TP4OpenW/lists/custom >> /var/lib/unbound/unbound_ext.conf
 sed "s/.*/forward-zone:\n        name: \"&\"\n        forward-addr: 127.0.0.1@45353/" /root/custom.list >> /var/lib/unbound/unbound_ext.conf
 
-/etc/init.d/unbound restart
+echo 'Generate /tmp/dnsmasq.d/gfwlist.conf...'
+echo '' > /tmp/dnsmasq.d/gfwlist.conf
+sed "s/.*/server=\/&\/127.0.0.1#45355/" /root/TP4OpenW/lists/list >> /tmp/dnsmasq.d/gfwlist.conf
+sed "s/.*/ipset=\/&\/gfwlist/" /root/TP4OpenW/lists/list >> /tmp/dnsmasq.d/gfwlist.conf
+
+sed "s/.*/server=\/&\/127.0.0.1#45355/" /root/TP4OpenW/lists/custom >> /tmp/dnsmasq.d/gfwlist.conf
+sed "s/.*/ipset=\/&\/gfwlist/" /root/TP4OpenW/lists/custom >> /tmp/dnsmasq.d/gfwlist.conf
+
+FILE=/root/custom.list
+if test -f "$FILE"; then
+  sed "s/.*/server=\/&\/127.0.0.1#45355/" /root/custom.list >> /tmp/dnsmasq.d/gfwlist.conf
+  sed "s/.*/ipset=\/&\/gfwlist/" /root/custom.list >> /tmp/dnsmasq.d/gfwlist.conf
+fi
+
+/etc/init.d/dnsmasq restart
 echo "Success."
 
