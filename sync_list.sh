9759a99719ba4e11711a3ada036fc5672fc75129
wang.yuan <wang.yuan@joydata.com>
2020-05-22 09:34:09 +0800

    update

--- sync_list.sh
+++ sync_list.sh
@@ -2,7 +2,7 @@
 
 rm -f /tmp/tmp/custom
 
-wget https://raw.githubusercontent.com/flyw/TP4OpenW/master/lists/custom  -O /tmp/tmp/custom -q
+wget https://raw.githubusercontent.com/flyw/TP4OpenW/master/lists/custom  -O /tmp/tmp/custom
 
 NEWFILE=/tmp/tmp/custom
 OLDFILE=/root/TP4OpenW/lists/custom
