85be8f2056fd54f937d5d71f2e77ec3fbb7474aa
wang.yuan <wang.yuan@joydata.com>
2020-05-25 11:00:09 +0800

    update

--- reinstall.sh
+++ reinstall.sh
@@ -6,9 +6,14 @@ fi
 
 rm -f /root/master.zip
 wget https://github.com/flyw/TP4OpenW/archive/master.zip -O /root/master.zip
-unzip /root/master.zip -d /root
-rm -rf /root/TP4OpenW
-sleep 1
-mv /root/TP4OpenW-master /root/TP4OpenW
-cp /root/TP4OpenW/etc/crontabs/root /etc/crontabs/root
-touch /root/custom.list
+if test -f "/root/master.zip"; then
+  unzip /root/master.zip -d /root
+  rm -rf /root/TP4OpenW
+  sleep 1
+  mv /root/TP4OpenW-master /root/TP4OpenW
+  cp /root/TP4OpenW/etc/crontabs/root /etc/crontabs/root
+  touch /root/custom.list
+fi
+
+
+
