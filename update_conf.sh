6a79e24808eb664af1a31acdfb2b0f279ea153d1
yuan <wang.yuan@joydata.com>
2025-11-10 16:36:31 +0800

    feat(dns): 更新屏蔽列表和自定义域名配置
    - 在 blocklist 中新增多个直播相关域名
    - 从 custom 列表中移除多个已不再需要的域名
    - 修改 update_conf.sh 脚本以优化屏蔽域名的处理方式
    - 将屏蔽域名指向本地回环地址实现拦截
    - 删除旧版屏蔽域名转发规则

--- update_conf.sh
+++ update_conf.sh
@@ -15,15 +15,17 @@
 
 # First clean up existing configuration
 echo '' > /var/lib/unbound/unbound_ext.conf
+# Add blocklist domains with forward-addr to a blackhole (localhost:53)
+# This effectively blocks these domains by sending them to nowhere
+sed '/./s/.*/local-zone: \"&\" always_nxdomain/' /root/TP4OpenW/lists/blocklist >> /var/lib/unbound/unbound_ext.conf
+#sed '/./s/.*/local-data: \"& A 0.0.0.0\"\nlocal-data: \"& AAAA ::1\"/' /root/TP4OpenW/lists/blocklist >> /var/lib/unbound/unbound_ext.conf
 
 # Add regular list domains
 sed "s/.*/forward-zone:\n        name: \"&\"\n        forward-addr: 127.0.0.1@45353/" /root/TP4OpenW/lists/list >> /var/lib/unbound/unbound_ext.conf
 sed "s/.*/forward-zone:\n        name: \"&\"\n        forward-addr: 127.0.0.1@45353/" /root/TP4OpenW/lists/custom >> /var/lib/unbound/unbound_ext.conf
 sed "s/.*/forward-zone:\n        name: \"&\"\n        forward-addr: 127.0.0.1@45353/" /root/custom.list >> /var/lib/unbound/unbound_ext.conf
 
-# Add blocklist domains with forward-addr to a blackhole (localhost:53)
-# This effectively blocks these domains by sending them to nowhere
-sed '/./s/.*/forward-zone:\n        name: \"&\"\n        forward-addr: 127.0.0.1@5353/' /root/TP4OpenW/lists/blocklist >> /var/lib/unbound/unbound_ext.conf
+
 
 echo 'Generate /tmp/dnsmasq.d/gfwlist.conf...'
 echo '' > /tmp/dnsmasq.d/gfwlist.conf
